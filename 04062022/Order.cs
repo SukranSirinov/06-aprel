da81c7e0641eb9a9c7db60e7f955faeaabe53ec0
Sukran <sukransirinov0@gmail.com>
2022-04-05 13:46:58 -0700

    6-aprel

---
+++ 04062022/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04062022
+{
+    internal class Order
+    {
+        static int _no;
+        public string No { get;  }
+        public int ProductCount { get; set; }
+        public int TotalAmout { get; set; }
+        public DateTime CreatedAt { get; set; } 
+        
+    }
+}
---
+++ 04062022/Shop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04062022
+{
+    internal class Shop
+    {
+        List<Order>Orders=new List<Order>();
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+        public double GetOrdersAvg()
+        {
+            double total=0;
+            foreach (var item in Orders)
+            {
+                total += item.TotalAmout;
+            }
+            total /= Orders.Count ;
+            return total;
+        }
+        public double GetOrdersAvg(DateTime date)
+        {
+            double total = 0;
+            var result=Orders.FindAll(x=>x.CreatedAt>=date);
+            foreach (var item in result)
+            {
+                total += item.TotalAmout;
+            }
+            total/=result.Count ;
+            return total;
+        }
+        public List<Order> FilterOrderByPrice(int minPrice,int maxPrice)
+        {
+            Orders.FindAll(x => x.TotalAmout > minPrice && x.TotalAmout < maxPrice);
+               return Orders;   
+        }
+        public void RemoveOrderByNo(string no)
+        {
+            if (!string.IsNullOrWhiteSpace(no))
+            {
+                throw new NullReferenceException("Null dir");
+
+            }
+            var result =Orders.Find(x =>x.No==no);
+            if (no == null)
+            {
+                throw new NullReferenceException("Null daxil edilib");
+            }
+            Orders.Remove(result);
+        }
+
+    }
+}
